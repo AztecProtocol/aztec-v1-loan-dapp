412ea8f6a38fc17271d899d21b0a3c3cb53d52ce
thomas-waite <thomas.waite4@gmail.com>
2020-03-10 11:52:06 +0000

    refactor: add factory migration scripts

--- migrations/2_ace.js
+++ migrations/2_ace.js
@@ -1,20 +1,20 @@
+/* globals artifacts */
 const ACE = artifacts.require('@aztec/protocol/contracts/ACE/ACE.sol');
-const AdjustSupply = artifacts.require('@aztec/protocol/contracts/ACE/validators/adjustSupply/AdjustSupply.sol');
-const BilateralSwap = artifacts.require('@aztec/protocol/contracts/ACE/validators/bilateralSwap/BilateralSwap.sol');
-const DividendComputation = artifacts.require('@aztec/protocol/contracts/ACE/validators/dividendComputation/DividendComputation.sol');
+const JoinSplitFluid = artifacts.require('@aztec/protocol/contracts/ACE/validators/joinSplitFluid/JoinSplitFluid.sol');
+const Swap = artifacts.require('@aztec/protocol/contracts/ACE/validators/swap/Swap.sol');
+const Dividend = artifacts.require('@aztec/protocol/contracts/ACE/validators/dividend/Dividend.sol');
 
 // remove all paths except name
 const JoinSplit = artifacts.require('@aztec/protocol/contracts/ACE/validators/joinSplit/JoinSplit.sol');
 const PrivateRange = artifacts.require('@aztec/protocol/contracts/ACE/validators/privateRange/PrivateRange.sol');
+const bn128 = require('@aztec/bn128');
 const utils = require('@aztec/dev-utils');
 
 const {
-  constants,
   proofs: {
-    BOGUS_PROOF,
     JOIN_SPLIT_PROOF,
     MINT_PROOF,
-    BILATERAL_SWAP_PROOF,
+    SWAP_PROOF,
     DIVIDEND_PROOF,
     PRIVATE_RANGE_PROOF,
   },
@@ -23,17 +23,17 @@ const {
 module.exports = async (deployer, network) => {
   if (network === 'development' || network === 'test') {
     await deployer.deploy(ACE);
-    await deployer.deploy(AdjustSupply);
-    await deployer.deploy(BilateralSwap);
+    await deployer.deploy(JoinSplitFluid);
+    await deployer.deploy(Swap);
     await deployer.deploy(JoinSplit);
-    await deployer.deploy(DividendComputation);
+    await deployer.deploy(Dividend);
     await deployer.deploy(PrivateRange);
     const ACEContract = await ACE.deployed();
-    const AdjustSupplyContract = await AdjustSupply.deployed();
-    await ACEContract.setCommonReferenceString(constants.CRS);
-    await ACEContract.setProof(MINT_PROOF, AdjustSupplyContract.address);
-    await ACEContract.setProof(BILATERAL_SWAP_PROOF, BilateralSwap.address);
-    await ACEContract.setProof(DIVIDEND_PROOF, DividendComputation.address);
+    const JoinSplitFluidContract = await JoinSplitFluid.deployed();
+    await ACEContract.setCommonReferenceString(bn128.CRS);
+    await ACEContract.setProof(MINT_PROOF, JoinSplitFluidContract.address);
+    await ACEContract.setProof(SWAP_PROOF, Swap.address);
+    await ACEContract.setProof(DIVIDEND_PROOF, Dividend.address);
     await ACEContract.setProof(JOIN_SPLIT_PROOF, JoinSplit.address);
     await ACEContract.setProof(PRIVATE_RANGE_PROOF, PrivateRange.address);
   }
